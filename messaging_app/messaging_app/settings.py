1693808fdc489bf9d2627c07bf8742b1e5570c95
Bright Atsighi <brytebee@gmail.com>
2025-07-22 10:49:47 +0100

    Added global pagination

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -129,7 +129,8 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer', # Enable the browsable API of DRF
     ],
-    'DEFAULT_PAGINATION_CLASS': 'chats.pagination.CustomLimitOffsetPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20,
     'DEFAULT_FILTER_BACKENDS': [
         'rest_framework.filters.SearchFilter',
         'rest_framework.filters.OrderingFilter',
