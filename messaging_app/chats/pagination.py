434c9f9d0d12b065ac619bfaa725361361f177b0
Bright Atsighi <brytebee@gmail.com>
2025-07-22 10:52:01 +0100

    Modified pagiantion file

--- messaging_app/chats/pagination.py
+++ messaging_app/chats/pagination.py
@@ -1,20 +1,21 @@
-from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
-class CustomLimitOffsetPagination(LimitOffsetPagination):
-    default_limit = 20
-    limit_query_param = 'limit'
-    offset_query_param = 'offset'
-    max_limit = 50
+class CustomPageNumberPagination(PageNumberPagination):
+    page_size = 20
+    page_size_query_param = 'page_size'
+    max_page_size = 50
+    page_query_param = 'page'
     
     def get_paginated_response(self, data):
         return Response({
             'pagination': {
                 'next': self.get_next_link(),
                 'previous': self.get_previous_link(),
-                'count': self.count,
-                'limit': self.limit,
-                'offset': self.offset
+                'count': self.page.paginator.count,
+                'total_pages': self.page.paginator.num_pages,
+                'current_page': self.page.number,
+                'page_size': self.page_size
             },
             'results': data
         })
